c75c0322128f110e68e75bfc6ebec148fda1625e
Jawad Mahmoud <2253483m@student.gla.ac.uk>
2018-02-27 22:58:57 +0000

    Created models for Show, Season and Episode

--- tvtrail/admin.py
+++ tvtrail/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tvtrail.models import tv_show, season, episode
 
 # Register your models here.
+
+admin.site.register(tv_show)
+admin.site.register(season)
+admin.site.register(episode)
--- tvtrail/models.py
+++ tvtrail/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class tv_show(models.Model):
+    show_id = models.CharField(max_length=128, unique=True)
+    show_name = models.CharField(max_length=512)
+    year = models.IntegerField(default=2000)
+    user_ratings = []
+    avg_rating = models.DecimalField(default=0, decimal_places=1, max_digits=2)
+    poster = models.ImageField()
+    genres = []
+    synopsis = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.show_name
+
+    class Meta:
+        verbose_name_plural = 'TV Shows'
+        verbose_name = 'TV Show'
+
+class season(models.Model):
+    season_id = models.CharField(max_length=128, unique=True)
+    show_id = models.ForeignKey(tv_show)
+    season_num = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.season_num
+
+class episode(models.Model):
+    epsiode_id = models.CharField(max_length=128, unique=True)
+    season_id = models.ForeignKey(season)
+    show_id = models.ForeignKey(tv_show)
+    episode_title = models.CharField(max_length=256)
+    episode_num = models.IntegerField(default=1)
+    user_ratings = []
+    avg_rating = models.DecimalField(default=0, decimal_places=1, max_digits=2)
+    runtime = models.IntegerField(default=20)
+
+    def __str__(self):
+        return self.episode_title
+
