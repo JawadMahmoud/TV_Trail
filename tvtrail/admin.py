d0c55743b781f8a86126e6ae68df5375344ba963
Jawad Mahmoud <2253483m@student.gla.ac.uk>
2018-03-02 23:26:35 +0000

    Updated models with experimental relationships between user profiles, shows and episodes.

--- tvtrail/admin.py
+++ tvtrail/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from tvtrail.models import tv_show, season, episode
+from tvtrail.models import tv_show, season, episode, genre, UserProfile
+from tvtrail.models import user_show_relation, user_episode_relation
 
 # Register your models here.
 
@@ -12,3 +13,7 @@ class episodeAdmin(admin.ModelAdmin):
 admin.site.register(tv_show, tv_showAdmin)
 admin.site.register(season)
 admin.site.register(episode, episodeAdmin)
+admin.site.register(genre)
+admin.site.register(UserProfile)
+admin.site.register(user_show_relation)
+admin.site.register(user_episode_relation)
--- tvtrail/models.py
+++ tvtrail/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 import datetime
 
 # Create your models here.
+
+class genre(models.Model):
+    genre_name = models.CharField(max_length=128, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.genre_name
 
 class tv_show(models.Model):
     show_id = models.CharField(primary_key=True, max_length=128, unique=True)
     show_name = models.CharField(max_length=512)
     year = models.IntegerField(default=2000)
-    user_ratings = []
     avg_rating = models.DecimalField(default=0, decimal_places=1, max_digits=2)
-    poster = models.ImageField()
-    genres = []
+    poster = models.ImageField(upload_to='series_posters', blank=True)
+    genres = models.ManyToManyField(genre)
     synopsis = models.TextField(max_length=10000)
     show_slug = models.SlugField()
 
@@ -40,7 +46,6 @@ class episode(models.Model):
     season_num = models.ForeignKey(season)
     episode_title = models.CharField(max_length=256)
     episode_num = models.IntegerField(default=1)
-    user_ratings = []
     avg_rating = models.DecimalField(default=0, decimal_places=1, max_digits=2)
     runtime = models.IntegerField(default=20)
     airdate = models.DateField(auto_now=False, auto_now_add=False, default=datetime.date.today)
@@ -54,3 +59,25 @@ class episode(models.Model):
     def __str__(self):
         return self.episode_title
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    watchlist = models.ManyToManyField(tv_show)
+    
+
+    def __str__(self):
+        return self.user.username
+
+class user_show_relation(models.Model):
+    user = models.ForeignKey(UserProfile)
+    show = models.ForeignKey(tv_show)
+    rating = models.DecimalField(default=0, decimal_places=1, max_digits=2)
+
+    def __str__(self):
+        return str(self.rating)
+
+class user_episode_relation(models.Model):
+    user = models.ForeignKey(UserProfile)
+    episode = models.ForeignKey(episode)
+    rating =  models.DecimalField(default=0, decimal_places=1, max_digits=2)
+    watched = models.BooleanField(default=False)
